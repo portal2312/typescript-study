d66fbd1b2fed45a287908328c99ed669bde7ed0d
portal2312 <portal2312@gmail.com>
2020-04-04 14:36:47 +0900

    Doing ch02-2

---
+++ ch02-2/index.ts
@@ -0,0 +1,15 @@
+let MAX_AGE = 100;
+// 
+// interface IPerson {
+//   name: string,
+//   age: number
+// }
+// 
+// class Person implements IPerson {
+//   constructor(public name: string, public age: number) {}
+// }
+// 
+// function makeRandomNumber(max: number = MAX_AGE): number {
+//   return Math.ceil((Math.random() * max));
+// }
+console.log(Math.random() * 100);
