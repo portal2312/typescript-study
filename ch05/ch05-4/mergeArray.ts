62bf20a0448ab9cfc227d5629de2356bad13cf13
portal2312 <portal2312@gmail.com>
2020-05-04 17:18:25 +0900

    Done ch05/ch05-4

---
+++ ch05/ch05-4/mergeArray.ts
@@ -0,0 +1,10 @@
+export const mergeArray = <T>(...arrays: readonly T[][]): T[] => {
+  let result: T[] = [];
+
+  for (let index = 0; index < arrays.length; index += 1) {
+    const array: T[] = arrays[index];
+    result = [...result, ...array];
+  }
+
+  return result;
+};
